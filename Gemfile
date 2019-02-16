cf9bc9532304271435c4f4c568c2b45e4c02eef5
Taketo Igai <t.igai.716@gmail.com>
2019-02-16 18:35:32 +0900

    add new files

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+gem 'anemone'
+gem 'nokogiri'
+gem 'mechanize'
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'launchy'
+gem 'csv'
+gem 'solargraph'
+
+# gem "rails"
---
+++ scrape_hotpepper_title.rb
@@ -0,0 +1,107 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'anemone'
+require 'kconv'
+require 'mechanize'
+require 'csv'
+
+time = Time.now.strftime('%Y%m%d-%H%M%S')
+header = ['name', 'url', 'drinks']
+url = 'https://www.hotpepper.jp/yoyaku/SA11/Y050/'
+
+CSV.open("result_ikebukuro_#{time}.csv", 'a') do |csv|
+  csv << header
+
+  Anemone.crawl(
+    url, skip_quary_strings: true, delay: 10, user_agent: 'Mac Safari 4'
+  ) do |anemone|
+    page_nm = 1
+    anemone.focus_crawl do |page|
+      page.links.keep_if do |link|
+        link.to_s.match('https://www.hotpepper.jp/yoyaku/SA11/Y050/bgn')
+      end
+    end
+
+    # あるエリアのページを巡回
+    anemone.on_every_page do |page|
+      puts "〜〜〜〜〜〜〜〜〜〜〜〜〜#{page_nm}目〜〜〜〜〜〜〜〜〜〜〜〜"
+      puts page.url
+
+      page_nm += 1
+      begin
+        html = URI(page.url).read
+
+        # 各ページのHTML解析
+        doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
+
+        store_array = []
+
+        # 店舗記載のブロックを取得
+        stores = doc.xpath(
+          "//div[@class='shopDetailText']"
+        )
+
+        stores.each do |store|
+          # 店舗名取得
+          store_name = store.xpath(
+            ".//a[@class='fs18 bold lh22 marB1']"
+          ).text
+
+          # 店舗id取得
+          store_id = store.xpath(
+            ".//a[@class='fs18 bold lh22 marB1']"
+          ).attribute('href').text
+
+          drink_url = "https://www.hotpepper.jp#{store_id}drink/"
+          drink_num = 1
+          drink_array = []
+          sleep(5)
+
+          begin
+            doc_drink = Nokogiri::HTML(URI(drink_url).read, nil, 'utf-8')
+
+            # drinkメニューHTML取得
+            drinks = doc_drink.xpath(
+              "//div[@class='shopInner']/h3"
+            )
+
+            drinks.each do
+              # drink名取得
+              drink_name = doc_drink.xpath(
+                "//h3[#{drink_num}]"
+              ).text
+
+              # drink値段取得
+              drink_price = doc_drink.xpath(
+                "//dl[@class='price' and position()=#{drink_num}]/dd"
+              ).text
+
+              drink = drink_name, [drink_price]
+              drink_array.push(drink)
+              drink_num += 1
+            end
+            sleep(5)
+            rescue => OpenURI::HTTPError
+              puts 'drinkページがないよー'
+              sleep(5)
+              next
+            end
+            store_data = [store_name, store_id, drink_array]
+            puts store_data
+            store_array.push(store_data)
+            store_array.each do |item|
+              csv << item
+            end
+          end
+      rescue => OpenURI::HTTPError
+        puts 'そんなことある！？'
+        sleep(5)
+        next
+      end
+      sleep(10)
+    end
+  end
+end
+
+puts '終了'
