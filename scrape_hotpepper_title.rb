8ca90cc3fd8f6e7bdb3b383b52a92a99f3657e95
Taketo Igai <t.igai.716@gmail.com>
2019-08-01 02:54:10 +0900

    ビールに絞った

--- scrape_hotpepper_title.rb
+++ scrape_hotpepper_title.rb
@@ -8,13 +8,18 @@ require 'csv'
 time = Time.now.strftime('%Y%m%d-%H%M%S')
 header = %w[name url drinks]
 url = 'https://www.hotpepper.jp/yoyaku/SA11/Y050/'
+# BEERS = /アサヒ|
+#         キリン|
+#         サッポロ|
+#         ヱビスビール|
+#         ザ・モルツ|
+#         ザ・プレミアム・モルツ|
+#         ビール/
 
 CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
   csv << header
 
-  Anemone.crawl(
-    url, skip_quary_strings: true, delay: 10, user_agent: 'Mac Safari 4'
-  ) do |anemone|
+  Anemone.crawl(url, skip_quary_strings: true, delay: 10, user_agent: 'Mac Safari 4') do |anemone|
     page_nm = 1
     anemone.focus_crawl do |page|
       page.links.keep_if do |link|
@@ -28,13 +33,11 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
       puts page.url
 
       page_nm += 1
-      begin
+       begin
         html = URI(page.url).read
 
         # 各ページのHTML解析
         doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
-
-        store_array = []
 
         # 店舗記載のブロックを取得
         stores = doc.xpath(
@@ -42,6 +45,7 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
         )
 
         stores.each do |store|
+          store_array = []
           # 店舗名取得
           store_name = store.xpath(
             ".//a[@class='fs18 bold lh22 marB1']"
@@ -52,32 +56,46 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
             ".//a[@class='fs18 bold lh22 marB1']"
           ).attribute('href').text
 
+          # drink_url = "https://www.hotpepper.jp#{store_id}drink/"
           drink_url = "https://www.hotpepper.jp#{store_id}drink/"
           drink_num = 1
           drink_array = []
+          # draft_beers = []
+          # low_malt_beers
           sleep(5)
 
           begin
               doc_drink = Nokogiri::HTML(URI(drink_url).read, nil, 'utf-8')
 
               # drinkメニューHTML取得
-              drinks = doc_drink.xpath(
+              drinks =  doc_drink.xpath(
                 "//div[@class='shopInner']/h3"
               )
 
-              drinks.each do
+              drinks.each do |drink|
                 # drink名取得
-                drink_name = doc_drink.xpath(
-                  "//h3[#{drink_num}]"
-                ).text
+                drink_name = p drink.text.gsub("\"", "")
+                # xpath(
+                #   "//h3[(contains(text(), 'ビール')) or (contains(text(), 'アサヒ')) or (contains(text(), 'キリン')) or (contains(text(), 'サッポロ')) or (contains(text(), 'ヱビスビール')) or (contains(text(), 'モルツ'))]"
+                # )
+
 
                 # drink値段取得
-                drink_price = doc_drink.xpath(
+                drink_price = p doc_drink.xpath(
                   "//dl[@class='price' and position()=#{drink_num}]/dd"
-                ).text
+                ).text.gsub("\"", "")
 
-                drink = drink_name, [drink_price]
-                drink_array.push(drink)
+                regex = /(\d{1,4})/
+                drink_price =~ regex
+                price_only_num = p $1.to_i
+
+                if (/ビール|アサヒ|キリン|サッポロ|ヱビスビール|エビス|モルツ/ =~ drink_name) &&
+                  (/ノンアルコール|ベース|ゼロ|フリー|零|甘太郎|クリア|ホップ|シャンディ|トマト|レッド|カシス|オレンジ|カンパリ/ !~ drink_name) &&
+                  (drink_name.length <= 25) &&
+                  (price_only_num != 0) &&
+                  (price_only_num <= 1000) then
+                    drink_array.push(drink_name, [drink_price])
+                end
                 drink_num += 1
               end
               sleep(5)
@@ -86,19 +104,16 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
             sleep(5)
             next
             end
-          store_data = [store_name, store_id, drink_array]
-          puts store_data
-          store_array.push(store_data)
-          puts store_array
+          store_array.push(store_name, store_id, drink_array.to_s.gsub(/^\[|\]$|\\|\"/, ""))
+          p store_array
+          csv << store_array
         end
-      rescue StandardError => OpenURI::HTTPError
-        puts 'そんなことある！？'
-        sleep(5)
-        next
-      end
-      store_array.each do |item|
-        csv << item
-      end
+      rescue => e
+      p e
+      puts 'そんなことある！？'
+      sleep(5)
+      next
+    end
       sleep(10)
     end
   end
