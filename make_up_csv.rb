4d97dd33a3dfa5e4a858a0137a356ecb0ffd7e79
Taketo Igai <t.igai.716@gmail.com>
2019-08-01 02:56:37 +0900

    ビールに絞った

--- make_up_csv.rb
+++ make_up_csv.rb
@@ -2,11 +2,11 @@ require 'csv'
 
 header = %w[name url drinks]
 rows = {}
-CSV.foreach('./lib/result_ikebukuro_oldFileName.csv', headers: true) do |row|
-  rows[row['url']] = row if !rows.key?(row['url']) 
+CSV.foreach('./lib/result_ikebukuro_20190731-023447.csv', headers: true) do |row|
+  rows[row['url']] = row if !rows.key?(row['url'])
 end
 
-CSV.open('./lib/new_result_ikebukuro_oldFileName.csv','w') do |newcsv|
+CSV.open('./lib/new_result_ikebukuro_20190731-023447.csv','w') do |newcsv|
   newcsv << header
    rows.each_value do |row|
       newcsv << row
