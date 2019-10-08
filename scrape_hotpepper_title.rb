cfb04495027196e5a5d0e0c52e106bfa0483e679
Taketo Igai <t.igai.716@gmail.com>
2019-10-08 15:56:07 +0900

    修正&ドリンク中心のデータに変更

--- scrape_hotpepper_title.rb
+++ scrape_hotpepper_title.rb
@@ -6,7 +6,7 @@ require 'kconv'
 require 'csv'
 
 time = Time.now.strftime('%Y%m%d-%H%M%S')
-header = %w[name url drinks]
+header = %w[name url drink price]
 url = 'https://www.hotpepper.jp/yoyaku/SA11/Y050/'
 # BEERS = /アサヒ|
 #         キリン|
@@ -45,27 +45,24 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
         )
 
         stores.each do |store|
-          store_array = []
           # 店舗名取得
           store_name = store.xpath(
             ".//a[@class='fs18 bold lh22 marB1']"
           ).text
 
           # 店舗id取得
-          store_id = store.xpath(
+          store_url = store.xpath(
             ".//a[@class='fs18 bold lh22 marB1']"
           ).attribute('href').text
 
-          # drink_url = "https://www.hotpepper.jp#{store_id}drink/"
-          drink_url = "https://www.hotpepper.jp#{store_id}drink/"
+          drink_url = "https://www.hotpepper.jp#{store_url}drink/"
           drink_num = 1
-          drink_array = []
-          # draft_beers = []
-          # low_malt_beers
           sleep(5)
 
           begin
               doc_drink = Nokogiri::HTML(URI(drink_url).read, nil, 'utf-8')
+
+              store_array = []
 
               # drinkメニューHTML取得
               drinks =  doc_drink.xpath(
@@ -74,40 +71,33 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
 
               drinks.each do |drink|
                 # drink名取得
-                drink_name = p drink.text.gsub("\"", "")
-                # xpath(
-                #   "//h3[(contains(text(), 'ビール')) or (contains(text(), 'アサヒ')) or (contains(text(), 'キリン')) or (contains(text(), 'サッポロ')) or (contains(text(), 'ヱビスビール')) or (contains(text(), 'モルツ'))]"
-                # )
+                drink_name = drink.text.gsub("\"", "")
 
-
-                # drink値段取得
-                drink_price = p doc_drink.xpath(
+                # drink値段取得かつ数字のみに変換
+                drink_price = doc_drink.xpath(
                   "//dl[@class='price' and position()=#{drink_num}]/dd"
-                ).text.gsub("\"", "")
+                ).text.gsub(/[^\d]/, '').to_i
 
-                regex = /(\d{1,4})/
-                drink_price =~ regex
-                price_only_num = p $1.to_i
-
+                # 一つ目の条件で入っててほしい単語を指定
+                # 二つ目の条件で入ってて欲しくない単語を指定
+                # 将来的には綺麗にしたい
                 if (/ビール|アサヒ|キリン|サッポロ|ヱビスビール|エビス|モルツ/ =~ drink_name) &&
                   (/ノンアルコール|ベース|ゼロ|フリー|零|甘太郎|クリア|ホップ|シャンディ|トマト|レッド|カシス|オレンジ|カンパリ/ !~ drink_name) &&
-                  (drink_name.length <= 25) &&
-                  (price_only_num != 0) &&
-                  (price_only_num <= 1000) then
-                    drink_array.push(drink_name, [drink_price])
+                  (drink_name.length <= 25) && # 25文字以上の生ビールないでしょ
+                  (drink_price != 0) && # priceがたまに0のがあるから排除
+                  (drink_price <= 1000) then #1000円以上のビールは飲み放題とかかぶるからなし
+                  p store_array.push(drink_name, drink_price, store_name, store_url)
+                  csv << store_array
+                  store_array = []
                 end
                 drink_num += 1
               end
-              sleep(5)
           rescue => e
             p e
             puts 'drinkページがないよー'
             sleep(5)
             next
             end
-          store_array.push(store_name, store_id, drink_array.to_s.gsub(/^\[|\]$|\\|\"/, ""))
-          p store_array
-          csv << store_array
         end
       rescue => e
       p e
@@ -115,7 +105,7 @@ CSV.open("./lib/result_ikebukuro_#{time}.csv", 'a') do |csv|
       sleep(5)
       next
     end
-      sleep(10)
+      sleep(5)
     end
   end
 end
